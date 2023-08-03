096f00218d0e682458cfea6ddba9440d8524d9a4
Jeevesh8 <creativityinczenyoga@gmail.com>
2023-08-03 17:16:07 +0530

    Log l2 l1 norms and commented code for aggregating

--- sparsity/calc_sparsity.py
+++ sparsity/calc_sparsity.py
@@ -30,10 +30,25 @@ with torch.no_grad():
     bias_l2_norm = torch.norm(top_layer.bias.data)
     bias_l1_norm = torch.norm(top_layer.bias.data, p=1)
 
-print(f"L2/L1 norm for seed {sys.argv[1]} at step 0:", ratio)
+print(f"L2 norm for seed {sys.argv[1]} at step 0:", l2_norm.item())
+print(f"L1 norm for seed {sys.argv[1]} at step 0:", l1_norm.item())
+print(f"L2/L1 norm for seed {sys.argv[1]} at step 0:", ratio.item())
 print(f"Bias for seed {sys.argv[1]} at step 0:", top_layer.bias.data)
-print(f"L2 Norm of bias: {bias_l2_norm}")
-print(f"L1 Norm of bias: {bias_l1_norm}")
+print(f"L2 Norm of bias: {bias_l2_norm.item()}")
+print(f"L1 Norm of bias: {bias_l1_norm.item()}")
 
 import shutil
 shutil.rmtree(local_dir)
+
+# import re
+# lis=[]
+# for i in range(100):
+#      with open(f"sbatch_outs/36027262_{i}.out") as f:
+#         for line in f.readlines():
+#             match_obj = re.fullmatch(r"L2/L1 norm for seed (\d+) at step 0: tensor\((.*)\)", line.strip())
+#             if match_obj is None:
+#                 continue
+#             seed = int(match_obj.group(1))
+#             ratio = float(match_obj.group(2))
+#             assert seed==i, f"{seed}!={i}"
+#             lis.append(ratio)
