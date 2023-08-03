7cc80d261d12690f726b1968f1691d71e6cf8c7b
Jeevesh8 <creativityinczenyoga@gmail.com>
2023-08-03 19:39:23 +0530

    Added svm for checking collapse of representation

---
+++ svm_plane/find_sep_plane.py
@@ -0,0 +1,35 @@
+import sys
+import pickle
+from sklearn import svm
+from sklearn.metrics import f1_score
+
+from datasets import load_dataset
+from transformers import BertTokenizer
+
+seed = sys.argv[1]
+steps = sys.argv[2]
+tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+paws_data = load_dataset("csv", data_files={"dev_and_test": "../paws_final/dev_and_test.tsv"}, delimiter="\t")["dev_and_test"]
+paws_data_to_labels = {}
+for sample in paws_data["dev_and_test"]:
+    ids = tokenizer(sample["sentence1"], sample["sentence2"], truncation=True, padding="max_length",return_tensors="pt")["input_ids"][0]
+    paws_data_to_labels[tokenizer.decode(ids[ids!=tokenizer.pad_token_id])] = sample["label"]
+paws_data = paws_data_to_labels
+
+def load_paws_data(pkl_file):
+    with open(pkl_file, "rb") as f:
+        reprs = pickle.load(f)["paws"]
+    Xs, Ys = [], []
+    for k, repr in reprs:
+        Xs.append(repr)
+        Ys.append(paws_data[k])
+    return Xs, Ys
+
+def get_f1(Xs, Ys):
+    clf = svm.SVC()
+    clf.fit(Xs, Ys)
+    Ypred = clf.predict(Xs)
+    return f1_score(Ys, Ypred)
+
+pkl_file = f"qqp_paws_embeds_{seed}_{steps}.pkl"
+print(f"F1 for {seed} at {steps}: {get_f1(*load_paws_data(pkl_file))}")
