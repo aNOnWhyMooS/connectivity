851efb844e7a9b38a6837fd97ffa68a9e0e7bff5
Jeevesh8 <creativityinczenyoga@gmail.com>
2023-12-10 23:03:16 +0530

    Added utility to delete models

---
+++ finetune/delete_model.py
@@ -0,0 +1,10 @@
+"""Delete a model on huggingface-hub.
+Usage:
+    python3 delete_model.py <hub_token> <repo_name>
+
+"""
+import sys
+from huggingface_hub import HfApi
+
+hf_api = HfApi(token=sys.argv[1])
+hf_api.delete_repo(sys.argv[2])
